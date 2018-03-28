08783b6921ea33c8e829fff592b85fb65bd78490
Grigory Entin <grigory.entin@gmail.com>
2018-03-28 20:11:44 +0200

    Added MasterItemCell.

---
+++ FlickrSearch/MasterItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  MasterItemCell.swift
+//  FlickrSearch
+//
+//  Created by Grigory Entin on 28.03.2018.
+//  Copyright © 2018 Grigory Entin. All rights reserved.
+//
+
+import UIKit.UICollectionViewCell
+
+class MasterItemCell : UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+}
