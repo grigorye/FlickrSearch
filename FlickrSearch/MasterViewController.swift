d938cf28e9501560e7ca13f81a1967305aa0dbd6
Grigory Entin <grigory.entin@gmail.com>
2018-03-30 17:05:46 +0200

    Switched to .photos; removed Core Data related stuff; added .searchResultsUpdater.

--- FlickrSearch/MasterViewController.swift
+++ FlickrSearch/MasterViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
-import CoreData
 
-class MasterViewController: UICollectionViewController, NSFetchedResultsControllerDelegate {
+typealias Photo = FlickrPhotosSearchResult.Photos.Photo
 
+class MasterViewController: UICollectionViewController, FlickrSearchResultsUpdaterDelegate {
+
+    lazy var searchResultsUpdater = FlickrSearchResultsUpdater() … {
+        $0.delegate = self
+    }
+    private var photos = [Photo]()
+    
     var detailViewController: DetailViewController? = nil
-    var managedObjectContext: NSManagedObjectContext? = nil
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +26,6 @@ class MasterViewController: UICollectionViewController, NSFetchedResultsControll
 
         collectionView.register(UINib(nibName: "MasterItemCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
 
-        // Do any additional setup after loading the view, typically from a nib.
-        navigationItem.leftBarButtonItem = editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -38,165 +37,74 @@ class MasterViewController: UICollectionViewController, NSFetchedResultsControll
         super.viewWillAppear(animated)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @objc
-    func insertNewObject(_ sender: Any) {
-        let context = self.fetchedResultsController.managedObjectContext
-        let newEvent = Event(context: context)
-
-        // If appropriate, configure the new managed object.
-        newEvent.timestamp = Date()
-
-        // Save the context.
-        do {
-            try context.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-    }
-
     // MARK: - Segues
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = collectionView?.indexPathsForSelectedItems?.last {
-                let object = fetchedResultsController.object(at: indexPath)
+                assert(indexPath.section == 0)
+                let object = photos[indexPath.row]
+                _ = x$(object)
+                #if false
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
+                #endif
             }
         }
     }
 
-    // MARK: - Table View
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return fetchedResultsController.sections?.count ?? 0
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let sectionInfo = fetchedResultsController.sections![section]
-        return sectionInfo.numberOfObjects
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        let event = fetchedResultsController.object(at: indexPath)
-        configureCell(cell, withEvent: event)
-        return cell
-    }
-
-
-    func configureCell(_ cell: UICollectionViewCell, withEvent event: Event) {
-        (cell as! MasterItemCell) … {
-            $0.textLabel.text = event.timestamp!.description
-        }
-    }
-
-    // MARK: - Fetched results controller
-
-    var fetchedResultsController: NSFetchedResultsController<Event> {
-        if _fetchedResultsController != nil {
-            return _fetchedResultsController!
-        }
-
-        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
-
-        // Set the batch size to a suitable number.
-        fetchRequest.fetchBatchSize = 20
-
-        // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
-
-        fetchRequest.sortDescriptors = [sortDescriptor]
-
-        // Edit the section name key path and cache name if appropriate.
-        // nil for section name key path means "no sections".
-        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
-        aFetchedResultsController.delegate = self
-        _fetchedResultsController = aFetchedResultsController
-
-        do {
-            try _fetchedResultsController!.performFetch()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-
-        return _fetchedResultsController!
-    }
-    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
-
-    var batchUpdates = [() -> Void]()
-
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        batchUpdates = []
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        guard let collectionView = collectionView else {
-            return
-        }
-        batchUpdates.append {
-            switch type {
-            case .insert:
-                collectionView.insertSections(IndexSet(integer: sectionIndex))
-            case .delete:
-                collectionView.deleteSections(IndexSet(integer: sectionIndex))
-            default:
-                assert(false)
-            }
-        }
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        guard let collectionView = self.collectionView else {
-            return
-        }
-        batchUpdates.append {
-            switch type {
-            case .insert:
-                collectionView.insertItems(at: [newIndexPath!])
-            case .delete:
-                collectionView.deleteItems(at: [indexPath!])
-            case .update:
-                collectionView.reloadItems(at: [indexPath!])
-            case .move:
-                collectionView.moveItem(at: indexPath!, to: newIndexPath!)
-            }
-        }
-    }
-
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        guard let collectionView = self.collectionView else {
-            return
-        }
+    // MARK: -
+    
+    func addMorePhotos(_ morePhotos: [Photo]) {
+        let collectionView = self.collectionView!
+        let oldPhotosCount = photos.count
+        photos += morePhotos
         collectionView.performBatchUpdates({
-            batchUpdates.forEach { $0() }
-        }, completion: { finished in
+            let indexPaths = (oldPhotosCount..<photos.count).map { IndexPath(item: $0, section: 0) }
+            collectionView.insertItems(at: indexPaths)
+        }, completion: { (finished) in
             _ = x$(finished)
         })
     }
+    
+    // MARK: - Flickr search results updater delegate
+    
+    func flickrSearchResultsUpdaterDidResetSearch(_ updater: FlickrSearchResultsUpdater) {
+        photos = []
+        collectionView?.reloadData()
+    }
+    
+    func flickrSearchResultsUpdater(_ updater: FlickrSearchResultsUpdater, didLoadMorePhotos morePhotos: [Photo]) {
+        DispatchQueue.main.async {
+            self.addMorePhotos(morePhotos)
+        }
+    }
 
-    /*
-     // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
+    // MARK: - Collection View
 
-     func controllerDidChangeContent(controller: NSFetchedResultsController) {
-         // In the simplest, most efficient, case, reload the table view.
-         collectionView?.reloadData()
-     }
-     */
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        assert(indexPath.section == 0)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        let photo = photos[indexPath.row]
+        configureCell(cell, withPhoto: photo)
+        return cell
+    }
+
+    func configureCell(_ cell: UICollectionViewCell, withPhoto photo: Photo) {
+        (cell as! MasterItemCell) … {
+            $0.textLabel.text = photo.id
+        }
+    }
 }
 
 extension MasterViewController : UICollectionViewDelegateFlowLayout {
