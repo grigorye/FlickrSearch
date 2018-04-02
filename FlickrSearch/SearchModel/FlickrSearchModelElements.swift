0f7f9fccf3274e8bcb5f955d3ba6ffb0a824d646
Grigory Entin <grigory.entin@gmail.com>
2018-04-03 00:17:15 +0200

    Added largeSquareURL.

--- FlickrSearch/SearchModel/FlickrSearchModelElements.swift
+++ FlickrSearch/SearchModel/FlickrSearchModelElements.swift
@@ -20,4 +20,9 @@ extension Photo {
         let url = URL(string: urlString)!
         return url
     }
+    var largeSquareURL: URL {
+        let urlString = "https://farm\(farm).staticflickr.com/\(server)/\(id)_\(secret)_q.jpg"
+        let url = URL(string: urlString)!
+        return url
+    }
 }
