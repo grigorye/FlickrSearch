be1cc5bbb12d350fc6f24eb5f33804c9deee6977
Grigory Entin <grigory.entin@gmail.com>
2018-04-02 17:52:58 +0200

    Added Photo.url.

--- FlickrSearch/SearchModel/FlickrSearchModelElements.swift
+++ FlickrSearch/SearchModel/FlickrSearchModelElements.swift
@@ -6,8 +6,18 @@
 //  Copyright © 2018 Grigory Entin. All rights reserved.
 //
 
+import Foundation.NSURL
+
 typealias Photo = FlickrPhotosSearchResult.Photos.Photo
 
 protocol PhotosDataSource {
     var photos: [Photo] { get }
+}
+
+extension Photo {
+    var url: URL {
+        let urlString = "https://farm\(farm).staticflickr.com/\(server)/\(id)_\(secret).jpg"
+        let url = URL(string: urlString)!
+        return url
+    }
 }
