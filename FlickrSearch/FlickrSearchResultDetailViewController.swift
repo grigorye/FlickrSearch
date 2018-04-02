980b68e2a05de65e8ddf3cc0d39eed3a9bdf607b
Grigory Entin <grigory.entin@gmail.com>
2018-04-02 17:54:45 +0200

    Added displaying/loading/caching the image.

--- FlickrSearch/FlickrSearchResultDetailViewController.swift
+++ FlickrSearch/FlickrSearchResultDetailViewController.swift
@@ -8,34 +8,38 @@
 
 import UIKit
 
+private let sessionConfiguration = URLSessionConfiguration.default … {
+    $0.urlCache = URLCache(memoryCapacity: URLCache.shared.memoryCapacity, diskCapacity: 1024 * 1024 * 100, diskPath: "FlickrSearchResultDetailView")
+}
+
+private let session = URLSession(configuration: sessionConfiguration, delegate: nil, delegateQueue: nil)
+
 class FlickrSearchResultDetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet private var imageView: UIImageView!
 
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let photo = photo {
-            if let label = detailDescriptionLabel {
-                label.text = photo.id
+    private func configureView() {
+        let dataTask = session.dataTask(with: photo.url) { (data, response, error) in
+            DispatchQueue.main.async {
+                setImageView(self.imageView, from: x$(data), x$(response), x$(error))
             }
+        }
+        if let cachedResponse = sessionConfiguration.urlCache?.cachedResponse(for: dataTask.currentRequest!) {
+            setImageView(imageView, from: x$(cachedResponse.data), x$(cachedResponse.response), nil)
+        } else {
+            x$(dataTask).resume()
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         configureView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    var photo: Photo? {
-        didSet {
-            // Update the view.
-            configureView()
+    var photo: Photo! {
+        willSet {
+            assert(photo == nil)
         }
     }
 }
