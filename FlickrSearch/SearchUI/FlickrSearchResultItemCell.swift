d047eab150f3fe9001f18bda4a6b80da0f19c13e
Grigory Entin <grigory.entin@gmail.com>
2018-04-02 17:52:37 +0200

    Switched to .photo; added displaying/loading/caching the image.

--- FlickrSearch/SearchUI/FlickrSearchResultItemCell.swift
+++ FlickrSearch/SearchUI/FlickrSearchResultItemCell.swift
@@ -9,9 +9,56 @@
 import UIKit.UICollectionViewCell
 import UIKit.UIImageView
 import UIKit.UILabel
+import Foundation
+
+private let sessionConfiguration = URLSessionConfiguration.default … {
+    $0.urlCache = URLCache(memoryCapacity: URLCache.shared.memoryCapacity, diskCapacity: 1024 * 1024 * 100, diskPath: "FlickrSearchResultItem")
+}
+
+private let session = URLSession(configuration: sessionConfiguration, delegate: nil, delegateQueue: nil)
 
 class FlickrSearchResultItemCell : UICollectionViewCell {
     
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var textLabel: UILabel!
+    private var dataTask: URLSessionDataTask!
+    
+    var photo: Photo! {
+        didSet {
+            textLabel.text = photo.title
+            var dataTask: URLSessionDataTask!
+            dataTask = session.dataTask(with: photo.url) { (data, response, error) in
+                guard dataTask! === self.dataTask else {
+                    assert((error as? URLError)?.code == .cancelled)
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.completePhotoLoad(x$(data), x$(response), x$(error))
+                }
+            }
+            self.dataTask = dataTask
+            if let cachedResponse = sessionConfiguration.urlCache?.cachedResponse(for: dataTask.currentRequest!) {
+                completePhotoLoad(x$(cachedResponse.data), x$(cachedResponse.response), nil)
+            } else {
+                x$(dataTask).resume()
+            }
+        }
+    }
+    
+    private func completePhotoLoad(_ data: Data?, _ response: URLResponse?, _ error: Error?) {
+        setImageView(imageView, from: data, response, error)
+    }
+    
+    // MARK: -
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        x$(dataTask)?.cancel()
+        dataTask = nil
+        imageView.image = nil
+    }
+    
+    // MARK: -
+    
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
 }
